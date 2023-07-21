5e6531433e46632b3cb75ccd84f8603f49e05ccb
joaquinbmateos <joaquin05tw@gmail.com>
2023-07-21 19:52:24 -0300

    add config file and dataprovider modifications

--- autdemo/src/test/java/automationdemo/tests/LoginTest.java
+++ autdemo/src/test/java/automationdemo/tests/LoginTest.java
@@ -1,5 +1,10 @@
 package automationdemo.tests;
 
+import org.json.JSONObject;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -15,7 +20,7 @@ public class LoginTest extends BaseTest{
     private static String expectedUrl = "https://automationexercise.com/login";
     private static String expectedLoginTitle = "Login to your account";
     
-    @Test(dataProvider = "validCredentials")
+    @Test(dataProvider = "userData")
     public void loginWithValidCredentials(String email, String password){
         homePage = new HomePage(driver);
         loginPage = new LoginPage(driver);
@@ -65,5 +70,19 @@ public class LoginTest extends BaseTest{
         return new Object[][]{
             {"test@automation.com", "seleniumjava"}
         };
+    }
+
+    @DataProvider(name = "userData")
+    public Object[][] getUserData() throws Exception {
+        String jsonContent = new String(Files.readAllBytes(Paths.get("src\\test\\java\\automationdemo\\utils\\config.json")));
+        JSONObject jsonObject = new JSONObject(jsonContent);
+        String userEmail = jsonObject.getString("userEmail");
+        String password = jsonObject.getString("password");
+
+        Object[][] data = new Object[1][2];
+        data[0][0] = userEmail;
+        data[0][1] = password;
+
+        return data;
     }
 }
